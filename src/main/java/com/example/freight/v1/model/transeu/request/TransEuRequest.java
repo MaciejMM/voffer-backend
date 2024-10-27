95588decf3f04a6b19ac0ee525a0bed6cd01906e
rw-MaciejMyslinski <maciej.myslinski@rockwool.com>
2024-10-27 20:15:53 +0100

    creating freight api req/res models

--- src/main/java/com/example/freight/v1/model/transeu/request/TransEuRequest.java
+++ src/main/java/com/example/freight/v1/model/transeu/request/TransEuRequest.java
@@ -3,14 +3,12 @@ package com.example.freight.v1.model.transeu.request;
 
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 public class TransEuRequest {
     private String vehicle_size;
     private String truck_body;
     private LoadingPlace loading_place;
-    private List<UnloadingPlaces> unloading_places;
+    private UnloadingPlace unloading_places;
     private Cargo cargo;
     private AvailableOn available_on;
 }
--- src/main/java/com/example/freight/v1/model/transeu/request/UnloadingPlace.java
+++ src/main/java/com/example/freight/v1/model/transeu/request/UnloadingPlace.java
@@ -3,7 +3,7 @@ package com.example.freight.v1.model.transeu.request;
 import lombok.Data;
 
 @Data
-public class UnloadingPlaces {
+public class UnloadingPlace {
 
     private TransEuAddress address;
 
