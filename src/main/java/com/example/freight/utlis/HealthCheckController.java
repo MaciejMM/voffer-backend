d2544a58afbd1f717db6e59f121e7a507b7dd70f
rw-MaciejMyslinski <maciej.myslinski@rockwool.com>
2024-11-03 17:07:20 +0100

    create health check response

--- src/main/java/com/example/freight/utlis/HealthCheckController.java
+++ src/main/java/com/example/freight/utlis/HealthCheckController.java
@@ -1,17 +1,20 @@
 package com.example.freight.utlis;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-public class HealthCheck {
+@RequestMapping(path = "/health-check", produces = MediaType.APPLICATION_JSON_VALUE)
+public class HealthCheckController {
 
 
-    @GetMapping
+    @GetMapping(value = "")
     public ResponseEntity<Map<String, Object>> healthCheck() {
         Map<String, Object> response = new HashMap<>();
         response.put("message", "OK");
