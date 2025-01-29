34817be155e0e5b044c816c27c6c87c2fc416d64
rw-MaciejMyslinski <maciej.myslinski@rockwool.com>
2025-01-29 16:42:51 +0100

    extend CORS, change ddl-auto

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-# Etap budowania
+# bulding image: docker build -t spring-boot-app .
 FROM gradle:8.10.2-jdk-21-and-22 AS build
 WORKDIR /app
 COPY . .
 RUN gradle build -x test
 
-# Etap uruchamiania
+# start from a base image
 FROM openjdk:21-jdk
 WORKDIR /app
 COPY --from=build /app/build/libs/*.jar app.jar
--- src/main/java/com/example/freight/config/CorsConfig.java
+++ src/main/java/com/example/freight/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     @Bean
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200","https://voffer-d18ce4ed1b53.herokuapp.com"));
         configuration.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
