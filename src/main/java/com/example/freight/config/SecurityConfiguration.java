282b0c62f751caa917f17c258d6a5f7e3b809618
rw-MaciejMyslinski <maciej.myslinski@rockwool.com>
2025-02-11 15:48:29 +0100

    wip - enable cors

--- src/main/java/com/example/freight/config/SecurityConfiguration.java
+++ src/main/java/com/example/freight/config/SecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -57,11 +58,12 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(
-                "http://localhost:4200",
-                "https://voffer-d18ce4ed1b53.herokuapp.com",
-                "https://voffer-lkkhkbrq52jbt3t.eu.kinde.com"
-        ));
+//        configuration.setAllowedOrigins(List.of(
+//                "http://localhost:4200",
+//                "https://voffer-d18ce4ed1b53.herokuapp.com",
+//                "https://voffer-lkkhkbrq52jbt3t.eu.kinde.com"
+//        ));
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
         configuration.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
